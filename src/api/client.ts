45c6fc802f13b9f286ef9ed3ec5c5c658bbf538a
jairom94 <jairom94@gmail.com>
2025-09-02 10:52:37 -0500

    fix: url local when not exist VITE_API_BASE_URL on .env file

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { HttpHeaders } from "./type-axios-headers";
 
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3001',
 });
 
 export const setAuthorizationHeader = (accessToken: string) => {
